d76b6d57b3e1ee79804dcf58353580ab3bc3578d
Advay Welling <93086932+advaywelling@users.noreply.github.com>
2026-07-16 23:37:27 -0400

    change param

--- Kernels/matrix_mult.cu
+++ Kernels/matrix_mult.cu
@@ -8,19 +8,18 @@
 
 #define TILE 16
 
-__global__ void matrix_mult(float *a, float *b, float *c, int M, int N, int K) {
+__global__ void naive_matrix_mult(float *a, float *b, float *c, int M, int K, int N) {
   int row = blockIdx.y * blockDim.y + threadIdx.y;
   int col = blockIdx.x * blockDim.x + threadIdx.x;
-  if (row < M && col < K) {
+  if (row < M && col < N) {
     float sum = 0;
-    for (int i{}; i < N; i++) {
-      sum += a[row * N + i] * b[i * K + col];
+    for (int i{}; i < K; i++) {
+      sum += a[row * K + i] * b[i * N + col];
     }
-    c[row * K + col] = sum;
+    c[row * N + col] = sum;
   }
 }
-
-__global__ void tiled_matrix_mult(float *a, float *b, float *c, int M, int N, int K) {
+__global__ void tiled_matrix_mult(float *a, float *b, float *c, int M, int K, int N) {
   __shared__ float share_a[TILE][TILE];
   __shared__ float share_b[TILE][TILE];
 
@@ -33,9 +32,9 @@ __global__ void tiled_matrix_mult(float *a, float *b, float *c, int M, int N, in
   int col = bx * blockDim.x + tx;
 
   float sum = 0.0f;
-  for(int i{}; i < (N + TILE - 1)/ TILE; i++) {
-    share_a[ty][tx] = (row < M && (i * TILE + tx) < N) ? a[(row * N) + (i * TILE + tx)] : 0.0f;
-    share_b[ty][tx] = ((i * TILE + ty) < N && col < K) ? b[((i * TILE + ty) * K + col)] : 0.0f;
+  for(int i{}; i < (K + TILE - 1)/ TILE; i++) {
+    share_a[ty][tx] = (row < M && (i * TILE + tx) < K) ? a[(row * K) + (i * TILE + tx)] : 0.0f;
+    share_b[ty][tx] = ((i * TILE + ty) < K && col < N) ? b[((i * TILE + ty) * N + col)] : 0.0f;
 
     __syncthreads();
 
@@ -44,12 +43,12 @@ __global__ void tiled_matrix_mult(float *a, float *b, float *c, int M, int N, in
     }
     __syncthreads();
   }
-  if (row < M && col < K) {
-    c[row * K + col] = sum;
+  if (row < M && col < N) {
+    c[row * N + col] = sum;
   }
 }
 
-void check_error(std::vector<float> &a, std::vector<float> &b, std::vector<float> &c, int M, int N, int K) {
+void check_error(std::vector<float> &a, std::vector<float> &b, std::vector<float> &c, int M, int K, int N) {
     for (int i{}; i < M; i++) {
         for (int j{}; j < K; j++) {
             float sum = 0;
@@ -68,21 +67,21 @@ int main() {
   constexpr int M = 1 << 10;
   constexpr int N = 1 << 10;
   constexpr int K = 1 << 10;
-  constexpr size_t size_A = sizeof(float) * M * N;
-  constexpr size_t size_B = sizeof(float) * N * K;
-  constexpr size_t size_C = sizeof(float) * M * K;
+  constexpr size_t size_A = sizeof(float) * M * K;
+  constexpr size_t size_B = sizeof(float) * K * N;
+  constexpr size_t size_C = sizeof(float) * M * N;
 
   std::vector<float> a;
-  a.reserve(M * N);
+  a.reserve(M * K);
   std::vector<float> b;
-  b.reserve(N * K);
+  b.reserve(K * N);
   std::vector<float> c;
-  c.resize(M * K); // fills with 0s
+  c.resize(M * N);
 
-  for (int i{}; i < M * N; i++) {
+  for (int i{}; i < M * K; i++) {
     a.push_back(rand() % 100);
   }
-  for (int i{}; i < N * K; i++) {
+  for (int i{}; i < K * N; i++) {
     b.push_back(rand() % 100);
   }
 
@@ -95,9 +94,9 @@ int main() {
   cudaMemcpy(d_b, b.data(), size_B, cudaMemcpyHostToDevice);
 
   dim3 dimBlock(TILE, TILE);
-  dim3 dimGrid((K + dimBlock.x - 1) / dimBlock.x,(M + dimBlock.y - 1) / dimBlock.y);
+  dim3 dimGrid((N + dimBlock.x - 1) / dimBlock.x,(M + dimBlock.y - 1) / dimBlock.y);
 
-  tiled_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, M, N, K);
+  tiled_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, M, K, N);
 
   cudaMemcpy(c.data(), d_c, size_C, cudaMemcpyDeviceToHost);
 
